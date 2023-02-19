10c8ae578d3523d6f35d8bce5b2419bae4e1f0c3
Ahmed Elsankary <elsankary99@gmail.com>
2023-02-19 20:19:19 +0200

    add form to note bottom sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:notes_app/views/widgets/notes_modal_bottom_sheet.dart';
+import 'package:notes_app/views/widgets/add_notes_modal_bottom_sheet.dart';
 
 import 'package:notes_app/views/widgets/notes_view_body.dart';
 
